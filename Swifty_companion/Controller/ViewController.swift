a3a34c8104330fe48b973d437d55b02f10a22fdd
Sydney <chnsydney@gmail.com>
2018-06-16 11:54:41 +0200

    "before oauth2test"

--- Swifty_companion/Controller/ViewController.swift
+++ Swifty_companion/Controller/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var UID : String = "730ef4263e45f77a5d91f469b9716ee036ce7eeb917c8b848e1c95eace5d25ad"
+    var SECRET : String = "45a66fa98d9a0a895797679b35eeeb7c9bef033fe3ce4f0e99bd3766c1fd1ed7"
     @IBOutlet weak var loginField: UITextField!
     @IBAction func searchButton(_ sender: Any) {
         print("searching")
